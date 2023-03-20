6d0358c79e8d0c0537c2f19c9386865ff479171f
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2023-03-20 17:12:46 +0300

    Added a Post counter

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -7,29 +7,37 @@ import java.util.*;
 
 @Repository
 public class PostRepository {
-  private final Map<Long, Post> posts;
+    private final Map<Long, Post> posts;
 
-  public PostRepository(Map<Long, Post> posts) {
-    this.posts = posts;
-  }
+    public PostRepository(Map<Long, Post> posts) {
+        this.posts = posts;
+    }
 
-  public List<Post> all() {
-    return Collections.unmodifiableList(new ArrayList<>(posts.values()));
-  }
+    public List<Post> all() {
+        return Collections.unmodifiableList(new ArrayList<>(posts.values()));
+    }
 
-  public Optional<Post> getById(long id) {
-    Post post = posts.get(id);
-    return Optional.ofNullable(post);
-  }
+    public Optional<Post> getById(long id) {
+        Post post = posts.get(id);
+        return Optional.ofNullable(post);
+    }
 
-  public Post save(Post post) {
-    String POST_CAN_NOT_BE_NULL = "Post cannot be null";
-    Objects.requireNonNull(post, POST_CAN_NOT_BE_NULL);
-    posts.put(post.getId(), post);
-    return post;
-  }
+    public Post save(Post post) {
+        String POST_CAN_NOT_BE_NULL = "Post cannot be null";
+        Objects.requireNonNull(post, POST_CAN_NOT_BE_NULL);
+        if (post.getId() == 0L) {
+            post.setId(posts.size() + 1L);
+        } else {
+            if (!posts.containsKey(post.getId())) {
+                throw new IllegalArgumentException("Cannot update non-existing post.");
+            }
+        }
 
-  public void removeById(long id) {
-    posts.remove(id);
-  }
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
