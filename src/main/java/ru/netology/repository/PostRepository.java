8a3176246f3fb845316d8d31ad0094694691d807
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2023-03-20 14:58:33 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,35 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.*;
+
+@Repository
+public class PostRepository {
+  private final Map<Long, Post> posts;
+
+  public PostRepository(Map<Long, Post> posts) {
+    this.posts = posts;
+  }
+
+  public List<Post> all() {
+    return Collections.unmodifiableList(new ArrayList<>(posts.values()));
+  }
+
+  public Optional<Post> getById(long id) {
+    Post post = posts.get(id);
+    return Optional.ofNullable(post);
+  }
+
+  public Post save(Post post) {
+    String POST_CAN_NOT_BE_NULL = "Post cannot be null";
+    Objects.requireNonNull(post, POST_CAN_NOT_BE_NULL);
+    posts.put(post.getId(), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    posts.remove(id);
+  }
+}
